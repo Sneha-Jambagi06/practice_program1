5244f35d3bdf4cee7c5a2c202096a7a59fda9280
Sneha-Jambagi06 <snehajambagi2020@gmail.com>
2025-11-27 14:22:05 +0530

    Update pract1.py

--- pract1.py
+++ pract1.py
@@ -1,11 +1,11 @@
 import sys
 if len(sys.argv)==6:
   script_name=sys.argv[0]
-  m1=sys.argv[1]
-  m2=sys.argv[2]
-  m3=sys.argv[3]
-  m4=sys.argv[4]
-  m5=sys.argv[5]
+  m1=int(sys.argv[1])
+  m2=int(sys.argv[2])
+  m3=int(sys.argv[3])
+  m4=int(sys.argv[4])
+  m5=int(sys.argv[5])
 else:
   script_name=sys.argv[0]
   m1=85
